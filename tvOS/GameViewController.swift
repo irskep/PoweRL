9ed7ec690c447187a3a89608836f6be01adf4a3c
Steve Johnson <steve@steveasleep.com>
2017-07-15 15:47:38 -0700

    Initial Commit

---
+++ tvOS/GameViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameViewController.swift
+//  tvOS
+//
+//  Created by Steve Johnson on 7/15/17.
+//  Copyright © 2017 Steve Johnson. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+}
